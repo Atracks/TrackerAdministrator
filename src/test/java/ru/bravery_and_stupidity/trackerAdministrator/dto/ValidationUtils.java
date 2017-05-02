9e2a4575e7b44216e332f05b0d63dc11f32460de
Atracks <Atracks@bk.ru>
2017-05-02 14:07:46 +0500

    Fix failing tests
    Adding isOrderEquals methods in ValidationUtils.java

--- src/test/java/ru/bravery_and_stupidity/trackerAdministrator/dto/ValidationUtils.java
+++ src/test/java/ru/bravery_and_stupidity/trackerAdministrator/dto/ValidationUtils.java
@@ -1,6 +1,7 @@
 package ru.bravery_and_stupidity.trackerAdministrator.dto;
 
 
+import ru.bravery_and_stupidity.trackerAdministrator.model.Order;
 import ru.bravery_and_stupidity.trackerAdministrator.model.Project;
 import ru.bravery_and_stupidity.trackerAdministrator.model.Task;
 import java.util.List;
@@ -19,7 +20,7 @@ final public class ValidationUtils {
     assertEquals(task.getIsOverdue(),taskDto.getIsOverdue());
     assertEquals(task.getParentTaskId(),taskDto.getParentTaskId());
     assertEquals(task.getStatus(),taskDto.getStatus());
-    assertTrue(task.getOrder().equals(taskDto.getOrder()));
+    ValidationUtils.isOrdersEquals(task.getOrder(),taskDto.getOrder());
     ValidationUtils.isProjectsEquals(task.getProject(),taskDto.getProject());
     assertTrue(task.getResponsible().equals(taskDto.getResponsible()));
     assertEquals(task.getImportance(),taskDto.getImportance());
@@ -41,6 +42,24 @@ final public class ValidationUtils {
     List<TaskDto> projectDtoTasks = projectDto.getTasks();
     for (int i = 0; i < project.getTasks().size() ; i++) {
       isTasksEquals((Task)projectTasks.toArray()[i], projectDtoTasks.get(i));
+    }
+  }
+
+  public static void isOrdersEquals(Order order, OrderDto orderDto) {
+    if((order.getIdOrder() == orderDto.getId())
+        &&(order.getDescription().equals(orderDto.getDescription()))) {
+    } else {throw new AssertionError("orders not equals");}
+  }
+
+  public static void isOrdersEquals(Order order, OrderWithTasksDto orderDto) {
+    if((order.getIdOrder() == orderDto.getId())
+        &&(order.getDescription().equals(orderDto.getDescription()))
+        &&(order.getTasks().size() == orderDto.getTasks().size())) {
+    } else {throw new AssertionError("orders not equals");}
+    Set<Task> orderTasks = order.getTasks();
+    List<TaskDto> orderDtoTasks = orderDto.getTasks();
+    for (int i = 0; i < order.getTasks().size() ; i++) {
+      isTasksEquals((Task)orderTasks.toArray()[i], orderDtoTasks.get(i));
     }
   }
 }
